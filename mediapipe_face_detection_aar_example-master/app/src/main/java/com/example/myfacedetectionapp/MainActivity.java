74d1d4d3eb160d59864bbde5a53fc29080e3a30a
蔡嘉亮 <alex@geetion.com>
2021-05-17 10:23:13 +0800

    update mediapipe v0.8.4  aar success!

--- mediapipe_face_detection_aar_example-master/app/src/main/java/com/example/myfacedetectionapp/MainActivity.java
+++ mediapipe_face_detection_aar_example-master/app/src/main/java/com/example/myfacedetectionapp/MainActivity.java
@@ -1,51 +1,71 @@
+// Copyright 2019 The MediaPipe Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.example.myfacedetectionapp;
 
 import android.graphics.SurfaceTexture;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.util.Log;
 import android.util.Size;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.google.mediapipe.components.CameraHelper;
 import com.google.mediapipe.components.CameraXPreviewHelper;
 import com.google.mediapipe.components.ExternalTextureConverter;
 import com.google.mediapipe.components.FrameProcessor;
 import com.google.mediapipe.components.PermissionHelper;
 import com.google.mediapipe.framework.AndroidAssetUtil;
-import com.google.mediapipe.framework.Packet;
 import com.google.mediapipe.glutil.EglManager;
 
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * Main activity of MediaPipe example apps.
- */
+/** Main activity of MediaPipe basic app. */
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
-    private static final String BINARY_GRAPH_NAME = "pb/face_effect_gpu.binarypb";
-    private static final String INPUT_VIDEO_STREAM_NAME = "input_video";
-    private static final String OUTPUT_VIDEO_STREAM_NAME = "output_video";
-    private static final CameraHelper.CameraFacing CAMERA_FACING = CameraHelper.CameraFacing.FRONT;
-
-    // Flips the camera-preview frames vertically before sending them into FrameProcessor to be
-    // processed in a MediaPipe graph, and flips the processed frames back when they are displayed.
-    // This is needed because OpenGL represents images assuming the image origin is at the bottom-left
-    // corner, whereas MediaPipe in general assumes the image origin is at top-left.
+    // Flips the camera-preview frames vertically by default, before sending them into FrameProcessor
+    // to be processed in a MediaPipe graph, and flips the processed frames back when they are
+    // displayed. This maybe needed because OpenGL represents images assuming the image origin is at
+    // the bottom-left corner, whereas MediaPipe in general assumes the image origin is at the
+    // top-left corner.
+    // NOTE: use "flipFramesVertically" in manifest metadata to override this behavior.
     private static final boolean FLIP_FRAMES_VERTICALLY = true;
+
+    // Number of output frames allocated in ExternalTextureConverter.
+    // NOTE: use "converterNumBuffers" in manifest metadata to override number of buffers. For
+    // example, when there is a FlowLimiterCalculator in the graph, number of buffers should be at
+    // least `max_in_flight + max_in_queue + 1` (where max_in_flight and max_in_queue are used in
+    // FlowLimiterCalculator options). That's because we need buffers for all the frames that are in
+    // flight/queue plus one for the next frame from the camera.
+    private static final int NUM_BUFFERS = 2;
 
     static {
         // Load all native libraries needed by the app.
         System.loadLibrary("mediapipe_jni");
-        System.loadLibrary("opencv_java3");
+        try {
+            System.loadLibrary("opencv_java3");
+        } catch (java.lang.UnsatisfiedLinkError e) {
+            // Some example apps (e.g. template matching) require OpenCV 4.
+            System.loadLibrary("opencv_java4");
+        }
     }
+
+    // Sends camera-preview frames into a MediaPipe graph for processing, and displays the processed
+    // frames onto a {@link Surface}.
+    protected FrameProcessor processor;
+    // Handles camera access via the {@link CameraX} Jetpack support library.
+    protected CameraXPreviewHelper cameraHelper;
 
     // {@link SurfaceTexture} where the camera-preview frames can be accessed.
     private SurfaceTexture previewFrameTexture;
@@ -54,31 +74,17 @@ public class MainActivity extends AppCompatActivity {
 
     // Creates and manages an {@link EGLContext}.
     private EglManager eglManager;
-    // Sends camera-preview frames into a MediaPipe graph for processing, and displays the processed
-    // frames onto a {@link Surface}.
-    protected FrameProcessor processor;
     // Converts the GL_TEXTURE_EXTERNAL_OES texture from Android camera into a regular texture to be
     // consumed by {@link FrameProcessor} and the underlying MediaPipe graph.
     private ExternalTextureConverter converter;
 
-    // Handles camera access via the {@link CameraX} Jetpack support library.
-    private CameraXPreviewHelper cameraHelper;
-    private static final String USE_FACE_DETECTION_INPUT_SOURCE_INPUT_SIDE_PACKET_NAME =
-            "use_face_detection_input_source";
-    private static final boolean USE_FACE_DETECTION_INPUT_SOURCE = false;
-
-    private static final String SELECTED_EFFECT_ID_INPUT_STREAM_NAME = "selected_effect_id";
-    private final Object effectSelectionLock = new Object();
-    private int selectedEffectId = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(getContentViewLayoutResId());
 
         previewDisplayView = new SurfaceView(this);
-
         setupPreviewDisplayView();
 
         // Initialize asset manager so that MediaPipe native libraries can access the app assets, e.g.,
@@ -86,44 +92,31 @@ public class MainActivity extends AppCompatActivity {
         AndroidAssetUtil.initializeNativeAssetManager(this);
         eglManager = new EglManager(null);
         processor =
-                new FrameProcessor(this,
+                new FrameProcessor(
+                        this,
                         eglManager.getNativeContext(),
-                        BINARY_GRAPH_NAME, INPUT_VIDEO_STREAM_NAME,
-                        OUTPUT_VIDEO_STREAM_NAME);
-        processor.getVideoSurfaceOutput().setFlipY(FLIP_FRAMES_VERTICALLY);
-        Map<String, Packet> inputSidePackets = new HashMap<>();
-        inputSidePackets.put(
-                USE_FACE_DETECTION_INPUT_SOURCE_INPUT_SIDE_PACKET_NAME,
-                processor.getPacketCreator().createBool(USE_FACE_DETECTION_INPUT_SOURCE));
-        processor.setInputSidePackets(inputSidePackets);
-        processor.setOnWillAddFrameListener(
-                (timestamp) -> {
-                    Packet selectedEffectIdPacket = null;
-                    try {
-                        synchronized (effectSelectionLock) {
-                            selectedEffectIdPacket = processor.getPacketCreator().createInt32(selectedEffectId);
-                        }
+                        "face_effect_gpu.binarypb",
+                        "input_video",
+                        "output_video");
+        processor
+                .getVideoSurfaceOutput()
+                .setFlipY(FLIP_FRAMES_VERTICALLY);
 
-                        processor
-                                .getGraph()
-                                .addPacketToInputStream(
-                                        SELECTED_EFFECT_ID_INPUT_STREAM_NAME, selectedEffectIdPacket, timestamp);
-                    } catch (RuntimeException e) {
-                        Log.e(
-                                TAG, "Exception while adding packet to input stream while switching effects: " + e);
-                    } finally {
-                        if (selectedEffectIdPacket != null) {
-                            selectedEffectIdPacket.release();
-                        }
-                    }
-                });
         PermissionHelper.checkAndRequestCameraPermissions(this);
+    }
+
+    // Used to obtain the content view for this application. If you are extending this class, and
+    // have a custom layout, override this method and return the custom layout.
+    protected int getContentViewLayoutResId() {
+        return R.layout.activity_main;
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        converter = new ExternalTextureConverter(eglManager.getContext());
+        converter =
+                new ExternalTextureConverter(
+                        eglManager.getContext(),NUM_BUFFERS);
         converter.setFlipY(FLIP_FRAMES_VERTICALLY);
         converter.setConsumer(processor);
         if (PermissionHelper.cameraPermissionsGranted(this)) {
@@ -135,6 +128,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         converter.close();
+
+        // Hide preview display until we re-open the camera again.
+        previewDisplayView.setVisibility(View.GONE);
     }
 
     @Override
@@ -144,10 +140,54 @@ public class MainActivity extends AppCompatActivity {
         PermissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
+    protected void onCameraStarted(SurfaceTexture surfaceTexture) {
+        previewFrameTexture = surfaceTexture;
+        // Make the display view visible to start showing the preview. This triggers the
+        // SurfaceHolder.Callback added to (the holder of) previewDisplayView.
+        previewDisplayView.setVisibility(View.VISIBLE);
+    }
+
+    protected Size cameraTargetResolution() {
+        return null; // No preference and let the camera (helper) decide.
+    }
+
+    public void startCamera() {
+        cameraHelper = new CameraXPreviewHelper();
+        cameraHelper.setOnCameraStartedListener(
+                surfaceTexture -> {
+                    onCameraStarted(surfaceTexture);
+                });
+        cameraHelper.startCamera(
+                this, CameraHelper.CameraFacing.FRONT, /*unusedSurfaceTexture=*/ null, cameraTargetResolution());
+    }
+
+    protected Size computeViewSize(int width, int height) {
+        return new Size(width, height);
+    }
+
+    protected void onPreviewDisplaySurfaceChanged(
+            SurfaceHolder holder, int format, int width, int height) {
+        // (Re-)Compute the ideal size of the camera-preview display (the area that the
+        // camera-preview frames get rendered onto, potentially with scaling and rotation)
+        // based on the size of the SurfaceView that contains the display.
+        Size viewSize = computeViewSize(width, height);
+        Size displaySize = cameraHelper.computeDisplaySizeFromViewSize(viewSize);
+        boolean isCameraRotated = cameraHelper.isCameraRotated();
+
+        // Connect the converter to the camera-preview frames as its input (via
+        // previewFrameTexture), and configure the output width and height as the computed
+        // display size.
+        converter.setSurfaceTextureAndAttachToGLContext(
+                previewFrameTexture,
+                isCameraRotated ? displaySize.getHeight() : displaySize.getWidth(),
+                isCameraRotated ? displaySize.getWidth() : displaySize.getHeight());
+    }
+
     private void setupPreviewDisplayView() {
         previewDisplayView.setVisibility(View.GONE);
         ViewGroup viewGroup = findViewById(R.id.preview_display_layout);
         viewGroup.addView(previewDisplayView);
+
         previewDisplayView
                 .getHolder()
                 .addCallback(
@@ -159,17 +199,7 @@ public class MainActivity extends AppCompatActivity {
 
                             @Override
                             public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-                                // (Re-)Compute the ideal size of the camera-preview display (the area that the
-                                // camera-preview frames get rendered onto, potentially with scaling and rotation)
-                                // based on the size of the SurfaceView that contains the display.
-                                Size viewSize = new Size(width, height);
-                                Size displaySize = cameraHelper.computeDisplaySizeFromViewSize(viewSize);
-
-                                // Connect the converter to the camera-preview frames as its input (via
-                                // previewFrameTexture), and configure the output width and height as the computed
-                                // display size.
-                                converter.setSurfaceTextureAndAttachToGLContext(
-                                        previewFrameTexture, displaySize.getWidth(), displaySize.getHeight());
+                                onPreviewDisplaySurfaceChanged(holder, format, width, height);
                             }
 
                             @Override
@@ -177,17 +207,5 @@ public class MainActivity extends AppCompatActivity {
                                 processor.getVideoSurfaceOutput().setSurface(null);
                             }
                         });
-    }
-
-    private void startCamera() {
-        cameraHelper = new CameraXPreviewHelper();
-        cameraHelper.setOnCameraStartedListener(
-                surfaceTexture -> {
-                    previewFrameTexture = surfaceTexture;
-                    // Make the display view visible to start showing the preview. This triggers the
-                    // SurfaceHolder.Callback added to (the holder of) previewDisplayView.
-                    previewDisplayView.setVisibility(View.VISIBLE);
-                });
-        cameraHelper.startCamera(this, CAMERA_FACING, /*surfaceTexture=*/ null,null);
     }
 }
